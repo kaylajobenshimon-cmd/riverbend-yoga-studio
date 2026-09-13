2967494dc0eaad462522f58fb1a3dcaf2924918a
kaylajobenshimon-cmd <kaylajobenshimon@gmail.com>
2026-09-13 08:50:39 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+const yogaClasses = [
+    {
+        preference: "gentle",
+        name: "Gentle Yoga",
+        description: "A slower class with simple movements and stretches."
+    },
+    {
+        preference: "active",
+        name: "Vinyasa Yoga",
+        description: "A more active class that connects movement with breathing."
+    },
+    {
+        preference: "relaxing",
+        name: "Restorative Yoga",
+        description: "A relaxing class focused on gentle poses and mindful breathing."
+    }
+];
